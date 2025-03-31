d2a5bc2be84da9c8488548d50008d8d7ed1ebe14
chawla-si <chawla.si@northeastern.edu>
2025-03-30 21:05:17 -0400

    Initial commit

---
+++ esd-6200/src/main/java/com/esd/esd_6200/Esd6200Application.java
@@ -0,0 +1,13 @@
+package com.esd.esd_6200;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esd6200Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esd6200Application.class, args);
+	}
+
+}
---
+++ esd-6200/src/test/java/com/esd/esd_6200/Esd6200ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esd.esd_6200;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esd6200ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
